3794dc05daba9f321b71a2d31efec6b8298831a0
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-12-03 19:04:34 -0600

    windowSizeClasses

---
+++ app/src/main/java/com/jaamcoding/composemaster/basic_layout/WindowSizeClassDemo.kt
@@ -0,0 +1,69 @@
+package com.jaamcoding.composemaster.basic_layout
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.window.core.layout.WindowWidthSizeClass
+import com.jaamcoding.composemaster.ui.theme.ComposeMasterTheme
+
+@Composable
+fun WindowSizeClassDemo() {
+    val windowClass = currentWindowAdaptiveInfo().windowSizeClass
+    Scaffold { innerPadding ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)) {
+            when (windowClass.windowWidthSizeClass) {
+                WindowWidthSizeClass.COMPACT,
+                WindowWidthSizeClass.MEDIUM -> {
+                    MyLazyList()
+
+                }
+
+                WindowWidthSizeClass.EXPANDED -> {
+                    Row(modifier = Modifier.fillMaxSize()){
+                        Column(modifier = Modifier.fillMaxHeight().weight(3f).background(Color.Red)) {
+                            Text("Menu item 1")
+                            Text("Menu item 2")
+                            Text("Menu item 3")
+                            Text("Menu item 4")
+                        }
+                        MyLazyList(modifier = Modifier.weight(7f).fillMaxHeight())
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyLazyList(modifier : Modifier = Modifier) {
+    LazyColumn(modifier = modifier.fillMaxSize()){
+        items(40){
+            Text(text = "Item $it", modifier = Modifier.padding(16.dp))
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Preview(showBackground = true)
+@Composable
+private fun WindowSizeClassDemoPrev() {
+    ComposeMasterTheme {
+        WindowSizeClassDemo()
+    }
+}
