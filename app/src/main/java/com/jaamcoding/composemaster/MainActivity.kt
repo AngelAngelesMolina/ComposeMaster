a31ee48c4f8c8e98684d52a5e8856f5db69da04c
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-12-05 18:25:32 -0600

    ADD: AssistChip

--- app/src/main/java/com/jaamcoding/composemaster/MainActivity.kt
+++ app/src/main/java/com/jaamcoding/composemaster/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jaamcoding.composemaster
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,19 +12,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jaamcoding.composemaster.basic_layout.HotelBookingScreen
 import com.jaamcoding.composemaster.ui.theme.ComposeMasterTheme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComposeMasterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    HotelBookingScreen()
                 }
             }
         }
--- app/src/main/java/com/jaamcoding/composemaster/basic_layout/HotelBookingScreen.kt
+++ app/src/main/java/com/jaamcoding/composemaster/basic_layout/HotelBookingScreen.kt
@@ -5,48 +5,135 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.window.core.layout.WindowWidthSizeClass
 import com.jaamcoding.composemaster.R
 import com.jaamcoding.composemaster.ui.theme.ComposeMasterTheme
 
+private val tags = listOf(
+    "City Center",
+    "Luxury",
+    "Instant Booking",
+    "Exclusive Deal",
+    "Early Bird Discount",
+    "Romantic Gateway",
+    "24/7 Support",
+
+    )
+
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun HotelBookingScreen() {
+fun HotelBookingScreen(paddingValues: PaddingValues = PaddingValues()) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .padding(paddingValues)
+            .background(Color.White),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
 
         item {
             Box() {
-                Image(painterResource(R.drawable.living_room), contentDescription = null)
+                Image(
+                    painterResource(R.drawable.living_room),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(max = 250.dp),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
                 HotelFadedBanner(
                     modifier = Modifier
                         .fillMaxWidth()
                         .align(Alignment.BottomCenter)
                 )
+            }
+        }
+        item {
+            HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+        }
+        item {
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(
+                    space = 40.dp,
+                    alignment = Alignment.CenterHorizontally
+                )
+            ) {
+                tags.forEach { tag ->
+                    AssistChip(onClick = {}, label = {
+                        Text(text = tag)
+                    })
+                }
+
+            }
+        }
+        item {
+            HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+        }
+        item {
+            Text(
+                text = "The advertisement features a vibrant and inviting design, showcasing the Hotel California Strawberry nestied in the heart of Los Angeles. Surrounded by the iconic Hollywood Sign, Griffith Park and stunning beaches, the hotel is perfectly located for guest to explore L.A's best attractions.".trimIndent(),
+                fontSize = 13.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+
+                    .padding(horizontal = 16.dp),
+                textAlign = TextAlign.Justify,
+            )
+        }
+        item {
+            Text(
+                text = "What we offer",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+        }
+        item {
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+
             }
         }
 
@@ -56,6 +143,7 @@ fun HotelBookingScreen() {
 @Preview(showBackground = true)
 @Composable
 fun HotelFadedBanner(modifier: Modifier = Modifier) {
+    val windowClass = currentWindowAdaptiveInfo().windowSizeClass
     Row(
         modifier = modifier
             .background(Color.White.copy(alpha = 0.7f))
@@ -67,14 +155,18 @@ fun HotelFadedBanner(modifier: Modifier = Modifier) {
 
             verticalArrangement = Arrangement.spacedBy(4.dp),
             modifier = Modifier
-                .background(Color.Red)
                 .weight(1f)
 
         ) {
             Text(
                 "Hotel California Strawberry",
                 fontWeight = FontWeight.Bold,
-                fontSize = 20.sp,
+                fontSize = when (windowClass.windowWidthSizeClass) {
+                    WindowWidthSizeClass.COMPACT -> 18.sp
+                    WindowWidthSizeClass.MEDIUM -> 24.sp
+                    WindowWidthSizeClass.EXPANDED -> 28.sp
+                    else -> 18.sp
+                },
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis,
             )
@@ -94,10 +186,26 @@ fun HotelFadedBanner(modifier: Modifier = Modifier) {
             })
         }
         Text(text = buildAnnotatedString {
-            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)) {
+            val fontSizeMultiplier = when (windowClass.windowWidthSizeClass) {
+                WindowWidthSizeClass.COMPACT -> 1f
+                WindowWidthSizeClass.MEDIUM -> 1.2f
+                WindowWidthSizeClass.EXPANDED -> 1.5f
+                else -> 1f
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp * fontSizeMultiplier
+                )
+            ) {
                 append("420$/")
             }
-            withStyle(style = SpanStyle(fontWeight = FontWeight.SemiBold, fontSize = 14.sp)) {
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp * fontSizeMultiplier
+                )
+            ) {
                 append("night")
             }
         })
@@ -127,6 +235,14 @@ private fun LabeledIcon(
 @Preview(showBackground = true)
 @Composable
 private fun HotelBookingScreenPrev() {
+    ComposeMasterTheme {
+        HotelBookingScreen()
+    }
+}
+
+@Preview(showBackground = true, device = Devices.NEXUS_10)
+@Composable
+private fun HotelBookingScreenTabletPrev() {
     ComposeMasterTheme {
         HotelBookingScreen()
     }
